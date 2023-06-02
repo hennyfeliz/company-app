5fe9719ae4ae056955bd52e56f16c0371489f3d6
Henny Feliz <66764084+hennyfeliz@users.noreply.github.com>
2023-06-02 19:24:40 -0400

    Implementando ultima cap de infraestructura de Employees

--- back/src/main/java/com/company/infraestructure/config/ApplicationConfig.java
+++ back/src/main/java/com/company/infraestructure/config/ApplicationConfig.java
@@ -1,16 +1,21 @@
 package com.company.infraestructure.config;
 
 import com.company.application.services.CompanyService;
+import com.company.application.services.EmployeeService;
 import com.company.application.usecases.CompanyUseCasesImpl;
+import com.company.application.usecases.EmployeUseCaseImpl;
 import com.company.domain.ports.out.CompanyRepositoryPort;
+import com.company.domain.ports.out.EmployeeRepositoryPort;
 import com.company.infraestructure.adapters.SomeExternalService;
 import com.company.infraestructure.repositories.Impl.JpaCompanyRepositoryAdapter;
+import com.company.infraestructure.repositories.Impl.JpaEmployeeRepositoryAdapter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApplicationConfig {
 
+  // Company
   @Bean
   public CompanyService companyService(CompanyRepositoryPort companyRepositoryPort){
     return new CompanyService(new CompanyUseCasesImpl(companyRepositoryPort));
@@ -20,6 +25,21 @@ public class ApplicationConfig {
   public CompanyRepositoryPort companyRepositoryPort(JpaCompanyRepositoryAdapter jpaCompanyRepositoryAdapter){
     return jpaCompanyRepositoryAdapter;
   }
+
+  // Employee
+
+  @Bean
+  public EmployeeService employeeService(EmployeeRepositoryPort employeeRepositoryPort){
+    return new EmployeeService(new EmployeUseCaseImpl(employeeRepositoryPort));
+  }
+
+  @Bean
+  public EmployeeRepositoryPort employeeRepositoryPort(JpaEmployeeRepositoryAdapter jpaEmployeeRepositoryAdapter){
+    return jpaEmployeeRepositoryAdapter;
+  }
+
+
+  // SomeExternalServices
 
   @Bean
   public SomeExternalService someExternalService(){
---
+++ back/src/main/java/com/company/infraestructure/controllers/EmployeeController.java
@@ -0,0 +1,54 @@
+package com.company.infraestructure.controllers;
+
+import com.company.application.services.EmployeeService;
+import com.company.domain.model.Employee;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+  private final EmployeeService employeeService;
+
+  public EmployeeController(EmployeeService employeeService) {
+    this.employeeService = employeeService;
+  }
+
+  @GetMapping("/")
+  public ResponseEntity<List<Employee>> getAllEmployees(){
+    List<Employee> employees = employeeService.getAllEmployees();
+    return new ResponseEntity<>(employees, HttpStatus.OK);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id){
+    return employeeService.getEmployeeById(id)
+            .map(employee -> new ResponseEntity<>(employee, HttpStatus.OK))
+            .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+  }
+
+  @PostMapping("/")
+  public ResponseEntity<Employee> createEmployee(@RequestBody Employee employee){
+    Employee createdEmployee = employeeService.createEmployee(employee);
+    return new ResponseEntity<>(createdEmployee, HttpStatus.CREATED);
+  }
+
+  @PutMapping("/")
+  public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employee){
+    return employeeService.updateEmployee(id, employee)
+            .map(employeen -> new ResponseEntity<>(employeen, HttpStatus.OK))
+            .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+  }
+
+  @DeleteMapping("/")
+  public ResponseEntity<String> deleteEmployee(@PathVariable Long id){
+    if(employeeService.deleteEmployee(id)){
+      return ResponseEntity.ok("Employee deleted");
+    } else{
+      return new ResponseEntity<>("Employee not found", HttpStatus.NOT_FOUND);
+    }
+  }
+}
