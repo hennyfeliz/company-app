632a3f0b4c5162eb4c9f3c9c5996b6d7577457e7
Henny Feliz <66764084+hennyfeliz@users.noreply.github.com>
2023-06-06 21:19:21 -0400

    Login Compañia

---
+++ front/src/Components/Login/Login/CompanyLogin/CompanyLogin.jsx
@@ -0,0 +1,37 @@
+import { Card } from "react-bootstrap";
+import "./CompanyLogin.css"
+import image from "./avatar.png"
+
+const CompanyLogin = () => {
+  return (
+    <>
+      <Card classNameName="m-5">
+        <div className="w-75 container">
+          <div className="login">
+            <div className="avatar">
+              <img src={image} />
+            </div>
+            <h2>Usuario existente</h2>
+            <h3>Bienvenid@ de vuelta</h3>
+
+            <form className="login-form">
+              <div className="textbox">
+                <input type="email" placeholder="Nombre de Usuario" />
+                <span className="material-symbols-outlined">  </span>
+              </div>
+              <div className="textbox">
+                <input type="password" placeholder="Contraseña" />
+                <span className="material-symbols-outlined">  </span>
+              </div>
+              <button type="submit">Entrar</button>
+              <a href="#">Olvidaste tus credenciales?</a>
+            </form>
+          </div>
+
+        </div>
+      </Card>
+    </>
+  )
+}
+
+export default CompanyLogin
