2a345c84ead4ea505757c940a9f6ebaf293b17f7
Henny Feliz <66764084+hennyfeliz@users.noreply.github.com>
2023-06-08 22:52:01 -0400

    Cambios navbar

--- front/src/Components/Navbar/Navbar.jsx
+++ front/src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 
 
-
+import "./NavbarStyles.css"
 
 const Navbar = () => {
   return (
@@ -35,7 +35,9 @@ const Navbar = () => {
           </ul>
           <form className="d-flex">
             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-            <button className="btn btn-outline-success" type="submit">Search</button>
+              <span className="user_icon">
+                <ion-icon name="person-circle-outline"></ion-icon>
+              </span>
           </form>
         </div>
       </div>
