00d3441e752dd2b353c3a3905a1b5621a330172e
Henny Feliz <66764084+hennyfeliz@users.noreply.github.com>
2023-06-10 15:10:02 -0400

    Busqueda por email para Users capa dominio

--- back/src/main/java/com/company/domain/ports/in/UserUseCase.java
+++ back/src/main/java/com/company/domain/ports/in/UserUseCase.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 public interface UserUseCase {
   User createUser(User user);
   Optional<User> getUserById(Long id);
+  Optional<User> getUserByEmail(String email);
   List<User> getAllUser();
   Optional<User> updateUser(Long id, User user);
   boolean deleteUser(Long id);
--- back/src/main/java/com/company/domain/ports/out/UserRepositoryPort.java
+++ back/src/main/java/com/company/domain/ports/out/UserRepositoryPort.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 public interface UserRepositoryPort {
   User save(User user);
   Optional<User> findById(Long id);
+  Optional<User> findByEmail(String email);
   List<User> findAll();
   Optional<User> update(User user);
   boolean deleteById(Long id);
