74902e1e4862188476bbda095720e36559ace16b
Henny Feliz <66764084+hennyfeliz@users.noreply.github.com>
2023-06-12 12:17:06 -0400

    Demás estilos para el contenido principal

--- front/src/Components/Principal/Principal.jsx
+++ front/src/Components/Principal/Principal.jsx
@@ -13,72 +13,72 @@ const Principal = () => {
       employeeName: "albert",
       employeeLastname: "feliz",
       company: {
-          id: 3,
-          companyName: "cocacola",
-          creationDate: "2020-01-01T00:00:00"
+        id: 3,
+        companyName: "cocacola",
+        creationDate: "2020-01-01T00:00:00"
       },
       contratationDate: "2020-01-01T00:00:00",
       position: {
-          id: 2,
-          positionName: "dev"
+        id: 2,
+        positionName: "dev"
       },
       team: {
-          id: 2,
-          teamName: "bcc"
+        id: 2,
+        teamName: "bcc"
       },
       user: {
-          id: 4,
-          email: "darling@gmail.com",
-          password: "darling",
-          username: "darling"
+        id: 4,
+        email: "darling@gmail.com",
+        password: "darling",
+        username: "darling"
       }
-    },    {
+    }, {
       id: 1,
       employeeName: "albert",
       employeeLastname: "feliz",
       company: {
-          id: 3,
-          companyName: "cocacola",
-          creationDate: "2020-01-01T00:00:00"
+        id: 3,
+        companyName: "cocacola",
+        creationDate: "2020-01-01T00:00:00"
       },
       contratationDate: "2020-01-01T00:00:00",
       position: {
-          id: 2,
-          positionName: "dev"
+        id: 2,
+        positionName: "dev"
       },
       team: {
-          id: 2,
-          teamName: "bcc"
+        id: 2,
+        teamName: "bcc"
       },
       user: {
-          id: 4,
-          email: "darling@gmail.com",
-          password: "darling",
-          username: "darling"
+        id: 4,
+        email: "darling@gmail.com",
+        password: "darling",
+        username: "darling"
       }
-    },    {
+    }, {
       id: 1,
       employeeName: "albert",
       employeeLastname: "feliz",
       company: {
-          id: 3,
-          companyName: "cocacola",
-          creationDate: "2020-01-01T00:00:00"
+        id: 3,
+        companyName: "cocacola",
+        creationDate: "2020-01-01T00:00:00"
       },
       contratationDate: "2020-01-01T00:00:00",
       position: {
-          id: 2,
-          positionName: "dev"
+        id: 2,
+        positionName: "dev"
       },
       team: {
-          id: 2,
-          teamName: "bcc"
+        id: 2,
+        teamName: "bcc"
       },
       user: {
-          id: 4,
-          email: "darling@gmail.com",
-          password: "darling",
-          username: "darling"
+        id: 4,
+        email: "darling@gmail.com",
+        password: "darling",
+        username: "darling"
       }
     },
   ]
@@ -87,39 +87,50 @@ const Principal = () => {
     <>
       {/* <LoginRegisterBodyComponent /> */}
       {/* <Login /> */}
-      <Container className="flex flex-column">
-      <div className="left_side">
-        <div className="title_element">
-          <h3 className="title">Personas a contratar</h3>
-        </div>
+      <Container className="flex d-flex">
+        <div className="left_side">
+          <div className="title_element">
+            <h3 className="title">Personas a contratar</h3>
+          </div>
 
-        {
-          elements.map((element) => {
-            return (
-              <>
-                <div className="elements">
-                  <span className="user_icon">
-                    <ion-icon name="person-circle-outline"></ion-icon>
-                  </span>
-                  <div className="titles">
-                    <span className="name">{`${element.employeeName} ${element.employeeLastname}` }</span>
-                    <span className="status">Contratado</span>
+          {
+            elements.map((element) => {
+              return (
+                <>
+                  <div className="elements">
+                    <span className="user_icon">
+                      <ion-icon name="person-circle-outline"></ion-icon>
+                    </span>
+                    <div className="titles">
+                      <span className="name">{`${element.employeeName} ${element.employeeLastname}`}</span>
+                      <span className="status">Contratado</span>
+                    </div>
+                    <span className="user_icon">
+                      <ion-icon name="chatbubble-ellipses-outline"></ion-icon>
+                    </span>
                   </div>
-                  <span className="user_icon">
-                    <ion-icon name="chatbubble-ellipses-outline"></ion-icon>
-                  </span>
-                </div>
-                <hr /> 
+                  <hr />
 
-              </>
-            )
-          })
-        }
+                </>
+              )
+            })
+          }
 
-      </div>
-      <div className="principal_content">
-        <h1>Hola</h1>
-      </div>
+        </div>
+        <div className="principal_content">
+          <div className="banner">
+
+          </div>
+          <div className="principal_content_icon">
+            <span className="user_icon_principal">
+              <ion-icon name="person-circle-outline"></ion-icon>
+            </span>
+          </div>
+          <div className="principal_content_info">
+
+          </div>
+          {/* <h1>Hola</h1> */}
+        </div>
       </Container>
     </>
   )
