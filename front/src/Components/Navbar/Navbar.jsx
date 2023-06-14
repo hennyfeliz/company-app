865fc2d7cb778ece4771c8c06cd48b0884ab02a1
Henny Feliz <66764084+hennyfeliz@users.noreply.github.com>
2023-06-13 22:45:25 -0400

    Cambios navbar

--- front/src/Components/Navbar/Navbar.jsx
+++ front/src/Components/Navbar/Navbar.jsx
@@ -18,26 +18,31 @@ const Navbar = () => {
             <li className="nav-item">
               <a className="nav-link" href="#">Link</a>
             </li>
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown
-              </a>
-              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li><a className="dropdown-item" href="#">Action</a></li>
-                <li><a className="dropdown-item" href="#">Another action</a></li>
-                <li><hr className="dropdown-divider" /></li>
-                <li><a className="dropdown-item" href="#">Something else here</a></li>
-              </ul>
-            </li>
+
             <li className="nav-item">
               <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
             </li>
           </ul>
           <form className="d-flex">
             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <span className="user_icon">
+            <div>
+              {/* <span className="user_icon">
                 <ion-icon name="person-circle-outline"></ion-icon>
-              </span>
+              </span> */}
+              <div className="nav-item dropdown">
+                <a className="nav-link" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  <span className="user_icon">
+                    <ion-icon name="person-circle-outline"></ion-icon>
+                  </span>
+                </a>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a className="dropdown-item" href="#">Action</a></li>
+                  <li><a className="dropdown-item" href="#">Another action</a></li>
+                  <li><hr className="dropdown-divider" /></li>
+                  <li><a className="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </div>
+            </div>
           </form>
         </div>
       </div>
